7e6cb61e59bae593a026e6e8cc99747fb2e502ad
fonzhousefonzie <harth.fonz@gmail.com>
2022-07-12 21:26:15 +0200

    Bonus

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <title>Document</title>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css' integrity='sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==' crossorigin='anonymous' />
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0-beta1/css/bootstrap.min.css' integrity='sha512-o/MhoRPVLExxZjCFVBsm17Pkztkzmh7Dp8k7/3JrtNCHh0AQ489kwpfA3dPSHzKDe8YCuEhxXq3Y71eb/o6amg==' crossorigin='anonymous' />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.js' integrity='sha512-pSyYzOKCLD2xoGM1GwkeHbdXgMRVsSqQaaUoHskx/HF09POwvow2VfVEdARIYwdeFLbu+2FCOTRYuiyeGxXkEg==' crossorigin='anonymous'></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/1.0.0-alpha.1/axios.min.js' integrity='sha512-xIPqqrfvUAc/Cspuj7Bq0UtHNo/5qkdyngx6Vwt+tmbvTLDszzXM0G6c91LXmGrRx8KEPulT+AfOOez+TeVylg==' crossorigin='anonymous'></script>
 </head>
@@ -19,8 +20,17 @@
         </header>
         <main>
             <div class="container">
+                <div class="dropdown pt-3">
+                    <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        Genre
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#" @click.prevent="resetList()">All</a></li>
+                        <li><a class="dropdown-item" href="#" v-for="genre in findGenres" :key="genre" @click.prevent="selectGenre(genre)">{{ genre }}</a></li>
+                    </ul>
+                </div>
                 <div class="row row-cols-2 row-cols-sm-5">
-                    <div class="col card border-0" v-for="song in songs" :key="song.title">
+                    <div class="col card border-0" v-for="song in selected" :key="song.title">
                         <div>
                             <img :src="song.poster">
                             <h5>{{ song.title }}</h5>
--- vue.js
+++ vue.js
@@ -1,12 +1,38 @@
 new Vue({
     el: "#app",
     data: {
-        songs: []
+        songs: [],
+        selected: []
     },
     mounted() {
         axios.get("db.php")
             .then(resp => {
                 this.songs = resp.data.songs;
+                this.selected = resp.data.songs;
             })
+    },
+    methods: {
+        selectGenre(genre) {
+            this.selected = [];
+            for (let i = 0; i < this.songs.length; i++) {
+                if (this.songs[i].genre === genre) {
+                    this.selected.push(this.songs[i]);
+                }
+            }
+        },
+        resetList(){
+            this.selected = this.songs;
+        }
+    },
+    computed: {
+        findGenres() {
+            const genreList = [];
+            for (let i = 0; i < this.songs.length; i++) {
+                if (!genreList.includes(this.songs[i].genre)) {
+                    genreList.push(this.songs[i].genre)
+                }
+            }
+            return genreList;
+        }
     }
 })
